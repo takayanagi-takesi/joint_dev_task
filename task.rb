4261c9c0d2bd744d337fd5affe8a398ec7c60df7
takayanagi <tatakekesisi9@gmail.com>
2021-01-14 03:09:54 +0900

    kadai 15

--- task.rb
+++ task.rb
@@ -141,12 +141,8 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-  if data1.key?(:age)
-    puts "OK"
-    else
-      puts "NG"
-  end
-  data2.key?(:age) ? "OK" : "NG"
+  puts data1.key?(:age) ? "OK" : "NG"
+  puts data2.key?(:age) ? "OK" : "NG"
 end
 
 def q16
