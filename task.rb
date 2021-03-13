fa214e329a4fa835890ccdb5633fc9a8a572a871
takayanagi <tatakekesisi9@gmail.com>
2021-03-13 11:36:13 +0900

    kadai20-6

--- task.rb
+++ task.rb
@@ -249,13 +249,13 @@ class Zoo
   def info_entry_fee(user)
     entry_fee = case user.age
   when 0..5
-    puts "#{user.name}の入場料金は#{entry_fee[:infant]}円です。"
+    puts "#{user.name}の入場料金は#{@entry_fee[:infant]}円です。"
   when 6..12
-    puts "#{user.name}の入場料金は#{entry_fee[:children]}円です。"
+    puts "#{user.name}の入場料金は#{@entry_fee[:children]}円です。"
   when 13..64
-    puts "#{user.name}の入場料金は#{entry_fee[:abult]}円です。"
+    puts "#{user.name}の入場料金は#{@entry_fee[:adult]}円です。"
   when 65..120
-    puts "#{user.name}の入場料金は#{entry_fee[:senior]}円です。"
+    puts "#{user.name}の入場料金は#{@entry_fee[:senior]}円です。"
   end
 end
 end
